029a6c73a4a879cb237c4c8352af61a815cc73d3
mortezaaghajaznadeh <mortezaaghajanzadeh@gmail.com>
2021-10-06 09:59:21 +0330

    Update ConnectedOwning-AllPairs.py

--- Codes/Python - Connected Stocks/ConnectedOwning-AllPairs.py
+++ Codes/Python - Connected Stocks/ConnectedOwning-AllPairs.py
@@ -71,7 +71,7 @@ def vv5(row):
 
 # %%
 path = r"E:\RA_Aghajanzadeh\Data\Connected_Stocks\\"
-# path = r"G:\Economics\Finance(Prof.Heidari-Aghajanzadeh)\Data\\"
+path = r"G:\Economics\Finance(Prof.Heidari-Aghajanzadeh)\Data\Connected stocks\\"
 
 df = pd.read_parquet(path + "Holder_Residual_1400_06_28.parquet")
 
