8de8b60d5b46aa56cfe792f18429fb6cfd08947c
Seyyed Morteza Aghajanzadeh <mortezaaghajanzadeh@gmail.com>
2021-11-09 10:40:32 +0330

    v1

--- Codes/Python - Connected Stocks/Run.py
+++
@@ -1,6 +0,0 @@
-#%%
-import os
-path = r"E:\RA_Aghajanzadeh\GitHub\Connected-Stocks\Codes\Python - Connected Stocks\\"
-os.system(path + '3.ConnectedOwning.py')
-os.system(path + '6.ConnectedOwning-AllPairs.py')
-# %%
