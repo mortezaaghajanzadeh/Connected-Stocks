b6c4503092ea183c664571325d89cb621b3d8f1a
Seyyed Morteza Aghajanzadeh <mortezaaghajanzadeh@gmail.com>
2021-12-21 09:54:33 +0330

    v1

--- Codes/Python - Connected Stocks/4.ConnectedOwning-sumup.py
+++ Codes/Python - Connected Stocks/4.ConnectedOwning-sumup.py
@@ -174,7 +174,7 @@ Monthly = pd.DataFrame()
 # %%
 path = r"E:\RA_Aghajanzadeh\Data\Connected_Stocks\NormalzedFCAP9.1_AllPairs\\"
 path2 = r"E:\RA_Aghajanzadeh\Data\Connected_Stocks\\"
-arr = os.listdir(path)
+arrs = os.listdir(path)
 tt = pd.read_parquet(path2 + "Holder_Residual_1400_06_28.parquet")
 tt["id"] = tt.id.astype(int)
 tt = tt[["symbol", "id"]].drop_duplicates()
